20ebac18eee3a4f9b8597219d0c0cad50b33f1c8
Taran Roth <taranr5080@gmail.com>
2022-11-21 12:40:55 -0500

    Finished project

--- index.js
+++ index.js
@@ -1,29 +1,60 @@
-var coinAmounts = new Array(4).fill(0);
+var coinToIndex = {
+    "pennies" : 0,
+    "nickels" : 1,
+    "dimes" : 2,
+    "quarters" : 3,
+    "halfdollars" : 4,
+    "dollars" : 5,
+}
+var indexToVal = {
+    0 : 1,
+    1 : 5,
+    2 : 10,
+    3 : 25,
+    4 : 50,
+    5 : 100,
+}
+var coins = ["pennies", "nickels", "dimes", "quarters", "halfdollars", "dollars"];
+var coinAmounts = new Array(coins.length).fill(0);
 
 function init() {
-    ["pennies", "nickels", "dimes", "quarters"].forEach((coin) => {
+    coins.forEach((coin) => {
         const capitalizedCoin = coin[0].toUpperCase() + coin.substring(1);
         $("body").append(`<div id="${coin}" class="money">
         <p id="${coin}Display">${capitalizedCoin}: 0</p>
+        <p id="${coin}Total">Total value of ${coin}: $0.00</p>
         <button class="change" onclick="changeCoin(${coin}, 1);">&plus;</button>
         <button class="change" onclick="changeCoin(${coin}, -1);">&minus;</button>
+        <br>
+        <button class="change" onclick="changeCoin(${coin}, 5);">&plus;5</button>
+        <button class="change" onclick="changeCoin(${coin}, -5);">&minus;5</button>
         </div>`);
     })
+    $("body").append(`<p id='total'>Total Value: $0.00</p>`);
 }
 
 function changeCoin(coin, amt) {
-    coinToIndex = {
-        "pennies" : 0,
-        "nickels" : 1,
-        "dimes" : 2,
-        "quarters" : 3,
-    }
-    coinVals = {
-        "pennies" : 1,
-        "nickels" : 5,
-        "dimes" : 10,
-        "quarters" : 25,
-    }
+    
     if (coinAmounts[coinToIndex[coin.id]] >= amt * -1) coinAmounts[coinToIndex[coin.id]] += amt;
     $(`#${coin.id}Display`).text(`${coin.id[0].toUpperCase() + coin.id.substring(1)}: ${coinAmounts[coinToIndex[coin.id]]}`)
+    updateTotalVal();
+}
+
+function updateTotalVal() {
+    var valList = [];
+    for (let i in indexToVal) {
+        valList.push(coinAmounts[i] * indexToVal[i]);
+    }
+    const total = valList.reduce((a, v) => {return a + v;}, 0);
+    for (i = 0; i < coins.length; i++) {
+        $(`#${coins[i]}Total`).text(`Total value of ${coins[i]}: ${valToMoney(valList[i])}`);
+    }
+    $('#total').text(`Total Value: ${valToMoney(total)}`);
+}
+
+function valToMoney(val) {
+    var dollars = Math.floor(val / 100);
+    var cents = val % 100;
+    if (cents < 10) cents = `0${cents}`;
+    return `$${dollars}.${cents}`;
 }
